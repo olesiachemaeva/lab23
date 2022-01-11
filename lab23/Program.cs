01fe51909bdc1dc703e4c6cd566544a446f200d3
chemaeva8898@gmail.com <chemaeva8898@gmail.com>
2022-01-11 22:56:11 +1000

    Добавьте файлы проекта.

---
+++ lab23/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace lab23
+{
+    class Program
+    {
+        static void Factorial()
+        {
+            int result = 1;
+            for (int i = 1; i <= 6; i++)
+            {
+                result *= i;
+            }
+            Thread.Sleep(3000);
+            Console.WriteLine($"Факториал равен {result}");
+        }
+        // определение асинхронного метода
+        static async void FactorialAsync()
+        {
+            Console.WriteLine("Начало метода Факториал"); // выполняется синхронно
+            await Task.Run(() => Factorial());                // выполняется асинхронно
+            Console.WriteLine("Конец метода Факториал");
+        }
+
+        static void Main(string[] args)
+        {
+            FactorialAsync();   // вызов асинхронного метода
+
+            Console.WriteLine("Введите число: ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Квадрат числа равен {n * n}");
+
+            Console.Read();
+        }
+    }
+}
